fc7c9ed0c6816ce311bd5a12d2956c040d2fb932
Giovanni Ghinet <ghinetgiovanni88@gmai.com>
2026-04-14 15:02:45 +0200

    usato il map per ottenere i nomi e metterli in un array

--- snack-2/script.js
+++ snack-2/script.js
@@ -4,5 +4,8 @@ const people = [
   { name: 'Marco', age: 67 }
 ];
 
+const nomi = people.map(person => person.name);
+console.log(nomi);
+
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
